d709c3879ed85578792bffe6e943a6c8660de652
Alex <alex@fs276ed17e.tkyc008.ap.nuro.jp>
2019-11-21 18:13:18 +0900

    first commit

---
+++ scripts/block.gd
@@ -0,0 +1,21 @@
+extends RigidBody
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+
+func _on_input_event(camera, event, click_position, click_normal, shape_idx):
+	print('hit')
+	if(event.is_pressed()):
+		get_node(".").queue_free()
+	pass # Replace with function body.
