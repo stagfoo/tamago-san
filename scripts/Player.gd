798ec9e6b5a3d092bc39f086628957cc7b3ec1f6
alex <alexanderddking@gmail.com>
2019-11-28 08:56:46 +0900

    Working face chnage

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,5 +1,7 @@
 extends RigidBody
 
+var currentFace = "default"
+var time = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,6 +10,22 @@ func _ready():
 func _on_RigidBody_body_entered(body):
 	var name = body.get_name()
 	print('Egg-san hit', name)
+	if(time == 0):
+		get_node("face").get_child(0).visible = false
+		get_node("face").get_child(1).visible = false
+		get_node("face").get_child(2).visible = false
+		get_node("face").get_child(3).visible = false
+		var faces = get_node("face").get_children()	
+		faces.shuffle()
+		faces[0].visible = true
+		time = 1
+	else:
+		get_node("face").get_child(0).visible = false
+		get_node("face").get_child(1).visible = false
+		get_node("face").get_child(2).visible = false
+		get_node("face").get_child(3).visible = false
+		get_node("face/default").visible = true
+		time = 0
 	if(name == 'table'):
 		# set var in GLOBAL
 		self.queue_free()
